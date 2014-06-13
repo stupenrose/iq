f6bfb8f751c6891ed117ce7a47f41ad2dc5d4746
Stuart Penrose <stu@penrose.us>
2014-06-12 21:17:27 -0700

    Jars for real

--- src/main/scala/org/iqbuild/Main.scala
+++ src/main/scala/org/iqbuild/Main.scala
@@ -11,6 +11,7 @@ import scala.collection.JavaConversions._
 import org.apache.commons.httpclient.HttpClient
 import org.apache.commons.httpclient.methods.GetMethod
 import java.io.FileOutputStream
+import org.apache.commons.io.FileUtils
 
 object Main {
   /*
@@ -28,9 +29,14 @@ object Main {
     
     object JarBuild extends BuildMechanism {
       override def build(path:File, dependencies:Seq[ResolvedDependency], m:ModuleDescriptor) {
+        val start = System.currentTimeMillis()
+        println("Working on " + path.getAbsolutePath())
         val sourceDir = new File(path, "src")
+        val javaFiles = new File(sourceDir, "java")
         val targetDir = new File(path, "target")
+        val stagingDir = new File(targetDir, "jar")
         targetDir.mkdirs()
+        stagingDir.mkdirs()
         
         val files = find(sourceDir)(_.getName().endsWith(".java")).toList
         
@@ -40,10 +46,31 @@ object Main {
         
         val classpath = dependencies.map(_.path .getAbsolutePath()).mkString(":")
         
+        val productFile = new File(targetDir, "product.jar")
         
-        exec(List("javac", "-d", targetDir.getAbsolutePath(), "-cp", classpath) ::: files.map(_.getAbsolutePath()), sourceDir)
+        println("Deleting " + stagingDir.getAbsolutePath())
+        FileUtils.deleteDirectory(stagingDir)
+        stagingDir.mkdirs()
+        
+        exec(List("javac", "-d", stagingDir.getAbsolutePath(), "-cp", classpath) ::: files.map(_.getAbsolutePath()), sourceDir)
+        val contents = find(stagingDir){i=>true}.toList
+        contents.foreach{f=>
+        	exec(List("jar", "-cvf", productFile.getAbsolutePath(), "-C", relativePath(stagingDir, path), relativePath(f, stagingDir)), path)
+        }
+        val end = System.currentTimeMillis()
+        val seconds = (end - start) /1000.0
+        println("Finished - " + seconds + " seconds")
       }
 
+      private def relativePath(child:File, parent:File) = {
+        val parentPath = parent.getAbsolutePath()
+        val childPath = child.getAbsolutePath()
+        if(childPath.startsWith(parentPath))
+          childPath.substring(parentPath.length()+1)
+        else
+          throw new RuntimeException()
+      }
+      
       private def exec(cmd: Seq[String], dir:File): Unit = {
         println(cmd.mkString(" "))
         
@@ -55,7 +82,8 @@ object Main {
         copy(p.getErrorStream(), System.err)
         
         val result = p.waitFor()
-        System.out.println("Exit " + result)
+        if(result!=0)
+        	throw new RuntimeException("Exit " + result)
       }
     }
     
